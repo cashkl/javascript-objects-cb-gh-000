9f275d94415699f91b928ae9c1ab5f9eb3f2cbc2
Murat Celik <cashkl@yandex.com>
2018-04-27 20:54:26 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {a:'b'}
 function updatePlaylist(playlist,artistName,songTitle) {
- retrun Object.assign(playlist,{[artistName]:songTitle})
+ return Object.assign(playlist,{[artistName]:songTitle})
 
 }
 function removeFromPlaylist(playlist,artistName) {
