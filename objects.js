d0b91fb3abc90f18d6e87a4786820be95f6af1a2
Murat Celik <cashkl@yandex.com>
2018-04-27 20:38:49 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,8 @@
-playlist = {cem karaca:'Incir Agaci'}
+playlist = {cem karaca:`Incir Agaci`}
 function updatePlaylist(object,name,title) {
   object.assign(object,[name]:title)
 }
 function updatePlaylist(object,name) {
   delete object.name
 }
+  
